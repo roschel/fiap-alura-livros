ea32237e63dd0c9c9e36e977e27ae249ebb6980c
roschel <joaomroschel@gmail.com>
2023-08-09 22:39:59 -0300

    evoluindo crud

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+
 const livros = [
   { id: 1, titulo: "O Senhor dos Anéis" },
   { id: 2, titulo: "O Hobbit" },
@@ -13,5 +16,30 @@ app.get("/", (req, res) => {
 app.get("/livros", (req, res) => {
   res.status(200).send(livros);
 });
+
+app.get("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+  res.status(201).json(livros[index]);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+
+  console.log(livros);
+
+  res.status(201).send("ok");
+});
+
+app.put("/livros/:id", (req, res) => {
+  let index = buscaLivro(req.params.id);
+
+  livros[index].titulo = req.body.titulo;
+
+  res.status(201).json(livros);
+});
+
+function buscaLivro(id) {
+  return livros.findIndex((livro) => livro.id == id);
+}
 
 export default app;
