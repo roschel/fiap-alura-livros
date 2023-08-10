a43d0ae4f98096e9d03ccc4292571c300614e77d
roschel <joaomroschel@gmail.com>
2023-08-09 22:11:58 -0300

    usando express

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const livros = [
+  { id: 1, titulo: "O Senhor dos Anéis" },
+  { id: 2, titulo: "O Hobbit" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Curso de Node");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).send(livros);
+});
+
+export default app;
