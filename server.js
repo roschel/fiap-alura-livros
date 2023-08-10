04e8c8cfa3b5f721130e75a59fc97dbbe866f677
roschel <joaomroschel@gmail.com>
2023-08-09 21:26:48 -0300

    iniciando api

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const port = 3000;
+
+const rotas = {
+  "/": "Curso de node",
+  "/livros": "Entrei na página de livros",
+  "/autores": "Listagem de autores",
+  "/editora": "Página de editores",
+  "/sobre": "Info sobre o projeto",
+};
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(rotas[req.url]);
+});
+
+server.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`);
+});
